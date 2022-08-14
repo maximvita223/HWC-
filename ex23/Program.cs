0e442f930f073bfc66a5b181b2c3f4a490d3bc14
Maxim <maks.bidenko.96@mail.ru>
2022-08-14 19:15:59 +0300

    fixed

--- ex23/Program.cs
+++ ex23/Program.cs
@@ -1,7 +1,10 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine());
 
-for (int x = 0; x <= n; x++) 
+for (int x = 1; x <= n; x++)
 {
     Console.WriteLine(Math.Pow(x, 3));
 }
