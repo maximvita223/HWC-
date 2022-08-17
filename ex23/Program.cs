46a424d08fb01d948352f675adb30c1a8c671c83
Maxim <maks.bidenko.96@mail.ru>
2022-08-17 16:54:38 +0300

    fixed

--- ex23/Program.cs
+++ ex23/Program.cs
@@ -3,8 +3,14 @@ Console.Clear();
 
 Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine()??"0");
-
+if (n<1)
+{
+    Console.WriteLine("Введите число от 1");
+}
+else
+{
 for (int x = 1; x <=Math.Abs(n); x++)
 {
     Console.WriteLine(Math.Pow(x, 3));
+}
 }
